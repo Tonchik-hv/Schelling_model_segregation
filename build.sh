13cdd31d106fa26dc465f24dd24158706054785a
Yuriyiv <you@example.com>
2023-10-29 19:15:21 +0300

    Build.sh is changed

--- build.sh
+++ build.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 echo "Starting build the project"
-run_script_with_sudo() {
-	if [ -x "$1" ]; then
-		echo "Executing $1 with sudo privileges..."
-		sudo ./"$1"
-		if [ $? -eq 0 ]; then
-			echo "$1 executed successfully."
-		else
-			echo "Error: $1 encountered an error during execution."
-			exit 1
-		fi
-	else
-		echo "Error: $1 does not have execute permission."
-		exit 1
-	fi
-}
 
 if ! command -v make &> /dev/null; then
 	echo "Installing make..."
@@ -22,8 +7,6 @@ if ! command -v make &> /dev/null; then
 	sudo apt-get install -y make
 fi
 
-run_script_with_sudo "prereqs.sh"
-run_script_with_sudo "install_python.sh"
 echo "Running make..."
 make
 
@@ -32,4 +15,3 @@ if [ $? -eq 0 ]; then
 else
 	echo "Error: Build process encountered an error."
 fi
-
